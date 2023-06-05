77fc203b3a87a947131cf1cf1035d91bd4baa4ac
Alexandre Ferreira de Lima <alexandre.ferreira1445@gmail.com>
2023-06-05 18:17:37 -0300

    🔨 Fix app run

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,11 @@ from src.app import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(
-        host=app.config.HOST,
-        port=app.config.PORT
-    )
+    
+    if app.config.FLASK_ENV == 'development':
+        app.run(
+            host=app.config.HOST,
+            port=app.config.PORT
+        )
+    else:
+        app.run()
